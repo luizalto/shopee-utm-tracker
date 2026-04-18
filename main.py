4fa2237157f07fdb2bc49e5ec49c0dddb82f1f88
luizds <102334218+luizalto@users.noreply.github.com>
2026-04-17 23:22:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -175,13 +175,10 @@ def click(request:Request,full_path:str):
     # =============================
 
     uc = request.query_params.get("uc", "default")
-    pos = request.query_params.get("pos", "Center")
 
-    if pos not in ["Left", "Middle", "Right"]:
-        pos = "Center"
 
     uc = clean(uc)
-    pos = clean(pos)
+    
 
     # =============================
     # UTM
